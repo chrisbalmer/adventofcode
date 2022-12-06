0a7770fa1f2093bb3d79ea983cee07d60a003c40
Chris Balmer <chris@balmerfamilyfarm.com>
2022-12-05 22:10:25 -0500

    finished day 3 part 2

--- 2022/day3/day3.go
+++ 2022/day3/day3.go
@@ -16,30 +16,45 @@ type Rucksack struct {
 	CompartmentA string
 	CompartmentB string
 	SharedItem   rune
+	Badge        rune
 }
 
 func main() {
 	rucksacks := readRucksacks(INPUT)
+	AddBadges(rucksacks)
 
 	priorityTotal := 0
+	badgePriorityTotal := 0
 	for i, rucksack := range rucksacks {
 		log.Printf("=================================")
 		log.Printf("Rucksack %d:", i)
 		log.Printf("All: %s", rucksack.All)
 		log.Printf("Compartment A: %s", rucksack.CompartmentA)
 		log.Printf("Compartment B: %s", rucksack.CompartmentB)
-		log.Printf("Shared Item: %q", rucksack.SharedItem)
 
 		priority, err := GetPriority(rucksack.SharedItem)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("Priority of Shared Item: %d\n\n", priority)
+		log.Printf("Shared Item: %q", rucksack.SharedItem)
+		log.Printf("Priority of Shared Item: %d", priority)
 		priorityTotal += priority
+
+		badgePriority, err := GetPriority(rucksack.Badge)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Badge: %q", rucksack.Badge)
+		log.Printf("Priority of Badge: %d\n\n", badgePriority)
+		if i%3 == 0 {
+			badgePriorityTotal += badgePriority
+		}
+
 	}
 
 	log.Printf("=================================")
-	log.Printf("Priority Total: %d\n\n", priorityTotal)
+	log.Printf("Priority Total: %d", priorityTotal)
+	log.Printf("Badge Priority Total: %d\n\n", badgePriorityTotal)
 }
 
 func readRucksacks(rucksackPackOutFile string) []Rucksack {
@@ -88,4 +103,20 @@ func GetPriority(r rune) (int, error) {
 	}
 
 	return -1, errors.New("unable to match shared item to a priority")
+}
+
+func AddBadges(rucksacks []Rucksack) {
+	for g := 0; g < len(rucksacks); g = g + 3 {
+		for _, letterA := range rucksacks[g].All {
+			for _, letterB := range rucksacks[g+1].All {
+				for _, letterC := range rucksacks[g+2].All {
+					if letterA == letterB && letterB == letterC {
+						rucksacks[g].Badge = letterA
+						rucksacks[g+1].Badge = letterA
+						rucksacks[g+2].Badge = letterA
+					}
+				}
+			}
+		}
+	}
 }
